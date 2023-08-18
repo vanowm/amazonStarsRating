54730936d91bfc3203534f99d86316c128c4deb9
V@no <git@vano.org>
2023-08-18 13:24:15 -0400

    added rating count

--- src/amazon-stars.user.js
+++ src/amazon-stars.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Amazon Stars
+// @name         Amazon stars rating
 // @namespace    V@no
 // @author       V@no
 // @description  Restore stars rating on Amazon product pages
-// @include      https://www.amazon.com/*
+// @include      https://www.amazon.*/*
 // @license      MIT
-// @version      1.0
+// @version      1.0.1
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
@@ -21,9 +21,9 @@ const processNode = node =>
 	if (!node.querySelectorAll) return;
 
 	const nlSingleStar = node.querySelectorAll(".puis-review-star-single");
-	for (let k = 0; k < nlSingleStar.length; k++)
+	for (let i = 0; i < nlSingleStar.length; i++)
 	{
-		const el = nlSingleStar[k];
+		const el = nlSingleStar[i];
 		const ratingFull = el.textContent;
 		const rating = ratingFull.split(".");
 		rating[1] = rating[1][0];
@@ -33,6 +33,15 @@ const processNode = node =>
 		const stars = rating[0] + (rating[1] > 2 && rating[1] < 8 ? "-5" : "");
 		el.classList.remove("puis-review-star-single", "a-star-small-5");
 		el.classList.add("a-star-small-" + stars);
+		//rating count
+		const elRatingCount = el.closest(".a-section").querySelector(".a-size-base.s-underline-text");
+		if (!elRatingCount.textContent)
+		{
+			const elRatingCountBox = elRatingCount.closest("[aria-label]");
+			elRatingCount.textContent = elRatingCountBox.ariaLabel;
+			const elFiveStarPercent = elRatingCountBox.nextSibling.firstChild.firstChild;
+			elFiveStarPercent.nodeValue = "(" + elFiveStarPercent.nodeValue + ")";
+		}
 	}
 };
 
@@ -48,4 +57,7 @@ new MutationObserver(mutations =>
 	}
 }).observe(document, { childList: true, subtree: true });
 
+document.addEventListener("DOMContentLoaded", () =>
+{
+});
 })();
